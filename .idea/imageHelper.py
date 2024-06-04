2b2eda064e17bcbcdf717db96b6366bbc5ba46ee
tagas <tagashira.1853@gmail.com>
2024-06-04 21:37:49 +0900

    ImageHelperUpdate

---
+++ .idea/imageHelper.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+
+class ImageHelper:
+
+    # CV画像を取得
+    def getCVImage(self, path):
+        return cv2.imread(path)
+    # Pillow画像を取得
+    def getPilImage(self, path):
+        return Image.open(path)
+
+    def save(self, path, image):
+        '''画像を保存する'''
+        cv2.imwrite(path, image)
+
+    # 画像サイズ変更
+    def resizeImage(self, width, height, path=None, image=None):
+        # リサイズ前の画像を読み込み
+        if path is not None:
+            resultImage = Image.open(path)
+        elif image is not None:
+            resultImage = image
+
+        # 横幅が大きければリサイズする
+        if (resultImage.width > int(width)):
+            resultImage = resultImage.resize((int(width), resultImage.height))
+        # 縦幅が大きければリサイズする
+        if (resultImage.height > int(height)):
+            resultImage = resultImage.resize((resultImage.width, int(height)))
+
+        return resultImage
+
+    def getReflectorPos(self, image, threshold=225):
+        '''反射板の位置を取得'''
+        #グレースケールにする
+        im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #閾値より白ければ反射板とする
+        ref_pos = np.where(im_gray > threshold)
+        result = []
+        #整理する
+        for x, y in zip(ref_pos[0], ref_pos[1]):
+            result.append((x, y))
+
+        return result
+
+    def trimming(self, image, left=0, right=0, top=0, bottom=0):
+        '''画像のトリミング'''
+        w, h = image.size
+        img_resize = image.crop((left, top, w - right, h - bottom))
+        return img_resize
+
+
+    # PillowをCVに変換
+    def pilTocv(self, image):
+        ''' PIL型 -> OpenCV型 '''
+        #最初からOpenCV型なら変更しない
+        if type(image) is np.ndarray:
+            return image
+        new_image = np.array(image, dtype=np.uint8)
+        if new_image.ndim == 2:  # モノクロ
+            pass
+        elif new_image.shape[2] == 3:  # カラー
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+        elif new_image.shape[2] == 4:  # 透過
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+        return new_image
+
+    # CVをPillowに変換
+    def cv2pil(self, image):
+        ''' OpenCV型 -> PIL型 '''
+        #最初からPIL型なら変更しない
+        if type(image) is PIL.Image.Image:
+            return image
+        new_image = image.copy()
+        if new_image.ndim == 2:  # モノクロ
+            pass
+        elif new_image.shape[2] == 3:  # カラー
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+        elif new_image.shape[2] == 4:  # 透過
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+        new_image = Image.fromarray(new_image)
+        return new_image
--- .idea/main.py
+++ .idea/main.py
@@ -1,12 +1,54 @@
 from mmpose.apis import MMPoseInferencer
+from PIL import Image
+import cv2
+from imageHelper import ImageHelper
+import os
 
-img_path = 'img_1.png'   # replace this with your own image path
+img_dir_source = './image/source/'
+img_dir_result = './image/result/'
+image_path_list = os.listdir(img_dir_source)
+ih = ImageHelper()
+for img_name in image_path_list:
+    img_path = img_dir_source + img_name   # replace this with your own image path
+    print(img_path)
 
-# instantiate the inferencer using the model alias
-inferencer = MMPoseInferencer('human')
+    #resize
+    # img = ih.getPilImage(img_path)
+    # img_resize = ih.trimming(img, left=700, right=700, top=400, bottom=400)
+    # img_resize_path = "./image/img_resize.jpg"
+    # img_resize.save(img_resize_path)
 
-# The MMPoseInferencer API employs a lazy inference approach,
-# creating a prediction generator when given input
-result_generator = inferencer(img_path, show=True)
-result = next(result_generator)
-print(result["predictions"][0][0]['keypoints'][0])
+    img_resize_path = img_path
+    # instantiate the inferencer using the model alias
+    inferencer = MMPoseInferencer('human')
+
+    # The MMPoseInferencer API employs a lazy inference approach,
+    # creating a prediction generator when given input
+    # result_generator = inferencer(img_path, show=True)
+    result_generator = inferencer(img_resize_path, show=True)
+    result = next(result_generator)
+    #ポイント
+    keypoints = result["predictions"][0][0]['keypoints']
+    keypoints_int = []
+    for pos in keypoints:
+        p = (int(pos[0]), int(pos[1]))
+        keypoints_int.append(p)
+    eye_left = keypoints_int[2]
+    eye_right = keypoints_int[3]
+    ear_left = keypoints_int[4]
+    ear_right = keypoints_int[5]
+
+    #cv2
+    img = cv2.imread(img_resize_path)
+
+    print(eye_left)
+    color = (0, 0, 255)
+    for pos in keypoints_int[1:5]:
+        img[pos[1] : pos[1] + 5, pos[0] : pos[0] + 5] = color
+        color = (0, 255, 0) if color == (0, 0, 255) else (0, 0, 255)
+    #反射板
+    ref_pos = ih.getReflectorPos(img)
+    for p in ref_pos:
+        img[p[0], p[1]] = (255, 0, 0)
+    # cv2.imshow("result", img)
+    cv2.imwrite(img_dir_result + img_name, img)
